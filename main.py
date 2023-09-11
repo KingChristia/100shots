472ddffc53336342795f5488ec44ad44dae20f3a
ChristianKartveit <chrigk@stud.ntnu.no>
2023-09-11 16:17:59 +1000

    changes for 0.33 and 0.5 liter boxes

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ if __name__ == '__main__':
     while shotnr < 100:
 
         say("Velkommen til 100 shots på 100 minutter")
+        print("Velkommen til 100 shots på 100 minutter")
         shotnr += 1
         shot = str(shotnr)
         print('Vi er på shot nr ' + shot)
@@ -24,9 +25,12 @@ if __name__ == '__main__':
         say(f'Ny shot om {wait} sekunder')
 
         if shotnr % 8 == 0:
-            say("Nå bør glassflaska snart være tom, en halv shot igjen")
-            print("Nå bør glassflaska snart være tom, en halv shot igjen")
-
+            say("Nå bør 0.33 liter glassflaska snart være tom, en halv shot igjen")
+            print("Nå bør 0.33 liter glassflaska snart være tom, en halv shot igjen")
+        
+        if shotnr % 12 == 0:
+            say("Nå bør 0.5 liter boksen være tom, en shot igjen")
+            print("Nå bør 0.5 liter boksen være tom, en shot igjen")
         sleep(wait)
 
     say("VI ER FEEEEERDIG")
